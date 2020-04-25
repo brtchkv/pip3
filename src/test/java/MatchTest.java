6463792c8bb0b92bda544d6cbe9aa3ee2d600a90
IvanB <ivan.bratchikov@yahoo.com>
2020-04-25 16:55:57 +0300

    testReport

--- src/test/java/MatchTest.java
+++ src/test/java/MatchTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class MatchingManagerTest {
+public class BatmanTest {
     private double r = 2;
 
     @BeforeClass
